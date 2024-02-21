6f05921ac0fef22c380bb7915956d4c285a0703e
Gustavo Lozada <zelote57@gmail.com>
2024-02-20 20:58:12 -0500

    add controllers

---
+++ Controllers/ArtistasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Data;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistasController : ControllerBase
+    {
+        private readonly MoviesDbContext _context;
+
+        public ArtistasController(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Artistas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Artista>>> GetArtistas()
+        {
+          if (_context.Artistas == null)
+          {
+              return NotFound();
+          }
+            return await _context.Artistas.ToListAsync();
+        }
+
+        // GET: api/Artistas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Artista>> GetArtista(int id)
+        {
+          if (_context.Artistas == null)
+          {
+              return NotFound();
+          }
+            var artista = await _context.Artistas.FindAsync(id);
+
+            if (artista == null)
+            {
+                return NotFound();
+            }
+
+            return artista;
+        }
+
+        // PUT: api/Artistas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArtista(int id, Artista artista)
+        {
+            if (id != artista.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(artista).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArtistaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Artistas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Artista>> PostArtista(Artista artista)
+        {
+          if (_context.Artistas == null)
+          {
+              return Problem("Entity set 'MoviesDbContext.Artistas'  is null.");
+          }
+            _context.Artistas.Add(artista);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArtista", new { id = artista.Id }, artista);
+        }
+
+        // DELETE: api/Artistas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArtista(int id)
+        {
+            if (_context.Artistas == null)
+            {
+                return NotFound();
+            }
+            var artista = await _context.Artistas.FindAsync(id);
+            if (artista == null)
+            {
+                return NotFound();
+            }
+
+            _context.Artistas.Remove(artista);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArtistaExists(int id)
+        {
+            return (_context.Artistas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/GenerosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Data;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        private readonly MoviesDbContext _context;
+
+        public GenerosController(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Generos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genero>>> GetGeneros()
+        {
+          if (_context.Generos == null)
+          {
+              return NotFound();
+          }
+            return await _context.Generos.ToListAsync();
+        }
+
+        // GET: api/Generos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genero>> GetGenero(int id)
+        {
+          if (_context.Generos == null)
+          {
+              return NotFound();
+          }
+            var genero = await _context.Generos.FindAsync(id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+        }
+
+        // PUT: api/Generos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenero(int id, Genero genero)
+        {
+            if (id != genero.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genero).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GeneroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Generos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Genero>> PostGenero(Genero genero)
+        {
+          if (_context.Generos == null)
+          {
+              return Problem("Entity set 'MoviesDbContext.Generos'  is null.");
+          }
+            _context.Generos.Add(genero);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGenero", new { id = genero.Id }, genero);
+        }
+
+        // DELETE: api/Generos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenero(int id)
+        {
+            if (_context.Generos == null)
+            {
+                return NotFound();
+            }
+            var genero = await _context.Generos.FindAsync(id);
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            _context.Generos.Remove(genero);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GeneroExists(int id)
+        {
+            return (_context.Generos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
